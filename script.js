237c32851414d4a0bea6aeac20eed677e0cb1407
Ali Saad <alisaad012@gmail.com>
2018-05-29 03:15:26 -0400

    resize with timeout

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 window.addEventListener('resize', function() {
-	const imgs = document.querySelectorAll('.image');
-	for (let i = 0; i < imgs.length; i++) {
-		imgs[i].classList.toggle('image');
-		imgs[i].classList.toggle('image');
-	}
+  const imgs = document.querySelectorAll('.image');
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].classList.toggle('image');
+  }
+  setTimeout(function() {
+    for (let i = 0; i < imgs.length; i++) {
+      imgs[i].classList.toggle('image');
+    }
+  }, .001);
 });
