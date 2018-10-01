b66620e1b4a37fce9d550afbba235243f354dd6e
Ali Saad <alisaad012@gmail.com>
2018-10-01 12:07:05 -0400

    Always shows images side by side

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-window.addEventListener('resize', function() {
-  const imgs = document.querySelectorAll('.image');
-  for (let i = 0; i < imgs.length; i++) {
-    imgs[i].classList.toggle('image');
+function resizeImages() {
+  const imgs = document.querySelector('#images');
+  const wide = window.innerWidth / window.innerHeight > 2 * 750 / 1072
+  // Remove the appropriate classes...
+  if (wide) {
+    imgs.classList.remove('horizontal');
+    imgs.classList.add('vertical');
+  } else {
+    imgs.classList.add('horizontal');
+    imgs.classList.remove('vertical')
   }
+  // ...then add them back after a while to reset
   setTimeout(function() {
-    for (let i = 0; i < imgs.length; i++) {
-      imgs[i].classList.toggle('image');
+    if (!wide) {
+      imgs.classList.remove('horizontal');
+      imgs.classList.add('vertical');
+    } else {
+      imgs.classList.add('horizontal');
+      imgs.classList.remove('vertical')
     }
   }, .001);
-});
+}
+resizeImages();
+window.addEventListener('resize', resizeImages);
